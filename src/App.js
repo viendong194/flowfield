ec556e483134767f3df5c045758dc32f8719688b
DONGVAN <viendong194@gmail.com>
2018-04-14 10:15:38 +0900

    initial setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  componentDidMount(){
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
